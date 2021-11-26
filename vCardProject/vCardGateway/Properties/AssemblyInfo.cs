563da3da55450f04edc5a68f3f2ef1b00893828a
Isac Amado <2191908@my.ipleiria.pt>
2021-11-26 15:43:42 +0000

    Added Bank Solution

--- vCardProject/vCardGateway/Properties/AssemblyInfo.cs
+++ vCardProject/vCardGateway/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("vCardProject")]
+[assembly: AssemblyTitle("vCardGateway")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("vCardProject")]
+[assembly: AssemblyProduct("vCardGateway")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4c2c9f55-e11a-4840-b53d-734e74ee02af")]
+[assembly: Guid("0f33cd6a-3333-4edd-b03d-9450375d9305")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
