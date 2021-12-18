bd85cf5a549e2d9ca31929fb607042c6263eaa5a
João Paulino <joaopaullino@gmail.com>
2021-12-18 10:41:19 +0000

    bank

---
+++ RestBank/RestBank/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestBank.Models
+{
+    public class Account
+    {
+        public int PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ConfirmationCode { get; set; }
+    }
+}
---
+++ RestBank/RestBank/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestBank.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
